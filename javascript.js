606f99f04e5c7d6ed2d1e2fbc220e1efc166be79
wroughtironkite <175876258+wroughtironkite@users.noreply.github.com>
2024-08-13 19:58:48 -0400

    Start project

---
+++ javascript.js
@@ -0,0 +1,19 @@
+function add(num1, num2) {
+
+}
+
+function subtract(num1, num2) {
+
+}
+
+function multiply(num1, num2) {
+
+}
+
+function divide(num1, num2) {
+
+}
+
+function solve(num1, num2, operand) {
+    
+}
